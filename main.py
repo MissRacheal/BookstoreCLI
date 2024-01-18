c37e46f2e818e550ab16678334f0bb7d5518d0b9
MissRacheal <33055218+MissRacheal@users.noreply.github.com>
2024-01-18 13:11:35 +0000

    Add files via upload
    The Updated Version of the Bookstore CLI in Python

--- main.py
+++ main.py
@@ -8,19 +8,28 @@ def main():
   books = []
  
  
+ 
+ #In the construction of this code third party code has been used
+ #to enhance and debug the program code. The following GitHub repository was used for
+ #the next section of the code. The original code and idea can be found at
+ # https://github.com/BekBrace/Python_work_flow_CLI_App/blob/main/main.py
+ #The code itself provides user input, allows users to lookup and display books.
+ #However, I have included the select a book in the options.
+ 
 
   #While loop allows users to navigate through menu options. 
   #The user must select 1-4, otherwise selecting 5 will quit the program
   #The condition is checked before each execution
   choice = 0
   while choice != 5 :
-    print("*** Welcome to Melanin's Bookstore. \n Kindly select from the options displayed by entering the numbers ")
+    print("*** Welcome to Melanin's Personalized BookShelf. \n Kindly select from the options displayed by entering the numbers: ")
     print("1) Add a Book")
     print("2) Lookup a Book")
     print("3) Select a Book")
     print("4) Display Books")
     print("5) Quit")
 
+    
     #Captures user input
     choice = int(input())
 
@@ -50,14 +59,24 @@ def main():
               print("Book not found.")
    
    
+   
+    #This section of code was debugged by the generative AI tool: CHATGPT.
     #conditional statement for selecting a book when the condition is true: entry is 3
     elif choice == 3:
-        print("Selecting a book....")
-        print(books)
-        print("Book has been selected successfully")
-
+     print(Listbook)
+     print("Selecting a book....")
+     #variable to store user input and converts to a lowercase
+     keyword = input("Enter the book to be selected: ").lower()
+     #it filters and searches through the List of book and does a comparison of the user input in lower case.
+     found_books = [book for book in Listbook if any(keyword in str(value).lower() for value in book)]
     
-
+     if found_books:
+        print("Book has been selected successfully:")
+        for book in found_books:
+            print(f"Title: {book[0]}, Author: {book[1]}, Pages: {book[2]}")
+     else:
+        print("Book not found.")
+    
     #Elif statement for displaying all books when the condition is true: entry is 4
     elif choice == 4:
       print("Displaying books.....")
@@ -68,13 +87,13 @@ def main():
     #Elif statement for quitting the application when the condition is true: entry is 5
     elif choice == 5:
       print("Quitting the application....")
-    print("Program Terminated")
+      print("Program Terminated")
 
 
 
 #it executes the main function if the name of the file correlates with the name of the main function
 if __name__ == "__main__":
-     main()
+    main()
 
 
     
